e96d71f7dede1e084cef3a215d27fbdb7c756133
hdood <m.bouguerzi1234@gmail.com>
2023-10-24 16:01:27 +0100

    fixing api url

---
+++ public/app/_nuxt/isLoggedIn.46deb4f2.js
@@ -0,0 +1,1 @@
+import{a0 as a,D as s,a1 as o,a2 as n}from"./entry.6ed29bba.js";const m=a(async(u,c)=>{let e,t;const r=s();try{return[e,t]=o(()=>r.getUser()),await e,t(),n("/admin")}catch{return!0}});export{m as default};
