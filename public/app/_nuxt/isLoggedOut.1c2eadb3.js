a37c2d0da35ff9360fca73bf2f9aa9b2ba151cf9
hdood <m.bouguerzi1234@gmail.com>
2023-10-24 18:59:23 +0100

    fix login middleware redirection

---
+++ public/app/_nuxt/isLoggedOut.1c2eadb3.js
@@ -0,0 +1,1 @@
+import{a0 as a,D as o,a1 as s,a2 as n}from"./entry.5b04ea27.js";const g=a(async(u,c)=>{let e,t;const r=o();try{[e,t]=s(()=>r.getUser()),await e,t()}catch{return n("/login")}});export{g as default};
