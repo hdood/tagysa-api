33de6c8e998f4f588eb2a52f99db39489c665b72
hdood <m.bouguerzi1234@gmail.com>
2023-10-28 12:11:45 +0100

    fixing landing page profile

---
+++ public/app/_nuxt/isLoggedOut.4625d583.js
@@ -0,0 +1,1 @@
+import{am as a,J as o,an as s,ad as n}from"./entry.3d28a381.js";const g=a(async(u,c)=>{let e,t;const r=o();try{[e,t]=s(()=>r.getUser()),await e,t()}catch{return n("/login")}});export{g as default};
