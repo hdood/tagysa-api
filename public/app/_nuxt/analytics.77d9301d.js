72ebd54e422a9abb688f23d9cb3d60b55253d8f3
hdood <m.bouguerzi1234@gmail.com>
2023-11-10 16:50:24 +0100

    fixing minor issues

---
+++ public/app/_nuxt/analytics.77d9301d.js
@@ -0,0 +1,1 @@
+const e={__name:"analytics",setup(t){return(_,a)=>null}};export{e as default};
--- public/app/_nuxt/isLoggedOut.e06e2093.js
+++
@@ -1,1 +0,0 @@
-import{am as a,J as o,an as s,ad as n}from"./entry.357d2ba9.js";const g=a(async(u,c)=>{let e,t;const r=o();try{[e,t]=s(()=>r.getUser()),await e,t()}catch{return n("/login")}});export{g as default};
